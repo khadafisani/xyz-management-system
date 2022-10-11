c6f660b5557984342d4f67117fb42beb3a3e3e74
khadafisani89 <khadafisani89@gmail.com>
2022-10-11 17:28:05 +0700

    Login & Logout feature

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -17,8 +19,33 @@ class AuthController extends Controller
         $user = User::create($data);
 
         return response()->api($user, 200, 'ok', 'Successfully register user');
-    } catch (Exception $e) {
+    } catch (\Exception $e) {
         return response()->api([], 400, 'error', 'Failed to register new user');
     }
+   }
+
+   public function login(LoginRequest $request)
+   {
+    $data = $request->validated();
+
+    try {
+        if(! Auth::attempt($data)) {
+            return response()->api([], 403, 'Email or password incorrect');
+        }
+
+        $data = [
+            'user' => auth()->user(),
+            'access_token' => auth()->user()->createToken('authToken')->accessToken
+        ];
+        return response()->api($data, 200, 'Successfully logged in');
+    }catch(\Exception $e) {
+        return response()->api([], 400, 'Failed to login');
+    }
+   }
+
+   public function logout()
+   {
+    auth()->user()->token()->revoke();
+    return response()->api([], 200, 'Successfully logged out');
    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers'], function () {
     Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
+
+    Route::middleware(['auth:api'])->group(function () {
+        Route::get('logout', 'AuthController@logout');
+    });
 });
