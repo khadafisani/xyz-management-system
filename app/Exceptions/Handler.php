db13615af6ea6155147a7e6b78a7b34a9dfef3dc
khadafisani89 <khadafisani89@gmail.com>
2022-10-17 18:45:59 +0700

    Handle exception via exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,29 +4,23 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
-    protected $levels = [
-        //
-    ];
-
-    /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<\Throwable>>
+     * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
         //
     ];
 
     /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
+     * A list of the inputs that are never flashed for validation exceptions.
      *
      * @var array<int, string>
      */
@@ -46,5 +40,29 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ValidationException) {
+            throw new HttpResponseException(response()->api($exception->errors(), 422, 'error'));
+        }
+
+        if ($exception instanceof ModelNotFoundException){
+            $position = strrpos($exception->getModel(), "\\");
+            $modelName = substr($exception->getModel(), $position+1);
+            $prettyName = ltrim(preg_replace('/[A-Z]/', ' $0', $modelName));
+            return response()->api([], 404, 'error', 'Sorry! '.$prettyName.' doesn\'t exist!');
+        }
+        return parent::render($request, $exception);
     }
 }
